f8c72c6dfbc1fdfa305cc5ce357472273ee4bd60
Jiaxin Cindy Tu <30844460+cindyhfls@users.noreply.github.com>
2021-08-15 20:32:57 -0500

    corrections

--- saveandloadmodels.py
+++ saveandloadmodels.py
@@ -2,4 +2,5 @@
 PATH = "simulated_model.pt"
 torch.save(net_baseline.state_dict(), PATH) 
  # load saved model
-torch.load('simulated_model.pt')
+net_baseline = Net(ncomp, NN1, NN2, bidi = True).to(device)
+net_baseline.load_state_dict(torch.load('simulated_model_baseline.pt'))
